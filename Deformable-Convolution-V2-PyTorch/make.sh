d4273c51e3b4e2ee0aa2723d9b83b312e8931ab9
jdonnelly36 <56931255+jdonnelly36@users.noreply.github.com>
2022-06-28 09:57:21 -0400

    Update make.sh

--- Deformable-Convolution-V2-PyTorch/make.sh
+++ Deformable-Convolution-V2-PyTorch/make.sh
@@ -1,13 +1,2 @@
-#!/usr/bin/env bash
-#SBATCH --job-name=proto_p_train  # Job name
-#SBATCH --mail-type=ALL          # Mail events (NONE, BEGIN, END, FAIL, ALL)
-#SBATCH --mail-user=jonathan.donnelly@maine.edu     # Where to send mail
-#SBATCH --output=testing_%j.out
-#SBATCH --ntasks=1                    # Run on a single Node
-#SBATCH --cpus-per-task=8
-#SBATCH --mem=4gb                     # Job memory request
-#SBATCH --time=1:00:00               # Time limit hrs:min:sec
-#SBATCH --partition=dgx
-#SBATCH --gres=gpu:1
 source /home/jdonnelly/protoPNet/bin/activate
 srun python setup.py build install 
